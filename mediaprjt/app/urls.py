f0834de01c571c414be8f3165e4f08a7bcf194c9
jithjayan <jithjayan9341@gmail.com>
2025-02-04 11:36:32 +0530

    new

---
+++ mediaprjt/app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns=[
+    path('',views.user_home),
+    path('admin_home',views.admin_home),
+    path('login',views.login),
+    
+
+]
---
+++ mediaprjt/app/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+
+# Create your views here.
+
+def login(req):
+
+    if req.method=='POST':
+        uname=req.POST['username']
+        password=req.POST['password']
+        data=authenticate(username=uname,password=password)
+        if data:
+            if data.is_superuser:
+                login(req,data)
+                req.session['admin']=uname
+                return redirect(admin_home)
+            else:
+                login(req,data)
+                req.session['user']=uname
+                return redirect(user_home)
+    else:
+        return render(req,'login.html')
+    
+
+
+# ----------------------------admin-------------------    
+    
+def admin_home(req):
+    return render(req,'admin_home.html')
+
+# ----------------------------user-------------------
+def user_home(req):
+    return render(req,'user/user_home.html')
